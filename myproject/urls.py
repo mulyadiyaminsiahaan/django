e254b248f8add99f4dd3677d47f2360a23bb4f94
mulyadiyaminsiahaan <112603524+mulyadiyaminsiahaan@users.noreply.github.com>
2025-03-11 14:14:00 +0700

    changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,7 +19,7 @@ from django.http import HttpResponse
 from django.urls import path
 
 def hello_world(request):
-    return HttpResponse("Hello, Welcom to Dimensi Kreasi Nusantara!!!")
+    return HttpResponse("Hello All, Ni Hao Ma, Horas, Welcome to Dimensi Kreasi Nusantara!!!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
